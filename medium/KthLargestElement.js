b7629cfb7c040ea1f36ffc1b8fe0a34405d5bbe8
Taite Jernigan <taite5100@gmail.com>
2021-12-26 11:17:33 -0800

    Completed second LeetCode problem

---
+++ medium/KthLargestElement.js
@@ -0,0 +1,23 @@
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// @param {number[]} nums
+// @param {number} k
+// @return {number}
+
+var findKthLargest = function(nums, k) {
+  nums.sort((a,b) => {
+    if (a > b) {
+      return -1;
+    } else if (a < b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  return nums[k - 1]
+};
+
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4))
+
