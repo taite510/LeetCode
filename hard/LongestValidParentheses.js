585be96a3d0571df3205cd00aa04c3e7334faadc
Taite Jernigan <taite5100@gmail.com>
2021-12-26 13:12:32 -0800

    Completed first hard LeetCode problem

---
+++ hard/LongestValidParentheses.js
@@ -0,0 +1,71 @@
+// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+// var longestValidParentheses = function(s) {
+//   var arr = [0];
+//   var validCounter = 0;
+//   var currentLength = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === '(') {
+//       validCounter++;
+//     }
+//     if (s[i] === ')') {
+//       validCounter--;
+//       if (validCounter >= 0) {
+//         currentLength += 2;
+//         arr.push(currentLength)
+//       } else {
+//         arr.push(currentLength);
+//         currentLength = 0;
+//         validCounter = 0;
+//       }
+//     }
+//   }
+//   arr.sort()
+//   console.log(arr)
+//   return arr[arr.length - 1]
+// };
+
+var longestValidParentheses = function(s) {
+  var arr = [0]
+  var currentLength = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      var validCounter = 0;
+      var length = 0;
+      for (let j = i; j < s.length; j++) {
+        if (s[j] === '(') {
+          validCounter++;
+          length++;
+        } else {
+          validCounter--;
+          length++;
+        }
+        if (validCounter === 0) {
+          currentLength += length;
+          arr.push(currentLength);
+          i = j
+          break
+        }
+        if (j === s.length - 1) {
+          arr.push(currentLength)
+          currentLength = 0;
+        }
+      }
+    } else {
+      currentLength = 0;
+    }
+  }
+  arr.sort((a,b) => {
+    if (a > b) {
+      return -1;
+    } else if (a < b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  return arr[0]
+}
+
+let string = "()(())"
+console.log(longestValidParentheses(string))
