7370c2c6fec8c7ed875fcb984abec522533d0cb4
Taite Jernigan <taite5100@gmail.com>
2021-12-31 13:12:34 -0800

    Completed leetcode problem

---
+++ easy/ClimbingStairs.js
@@ -0,0 +1,34 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+var climbStairs = function(n) {
+  let options = 0;
+  let func = (currentNum) => {
+    currentNum -= 1;
+    if (currentNum === 0) {
+      options++;
+      return
+    }
+    func(currentNum)
+    currentNum -= 1;
+    if (currentNum === 0) {
+      options++;
+      return
+    }
+    func(currentNum)
+  }
+  func(n)
+  return options
+};
+
+//faster solution I found online:
+
+var climbStairs2 = function(n) {
+  a = b = 1
+  while (n--)
+      a = (b += a) - a
+  return a
+};
+
+console.log(climbStairs2(45))
