8624a49849f9c15afd86f7d0b19de31b97a3de0a
Taite Jernigan <taite5100@gmail.com>
2022-01-01 13:47:36 -0800

    Completed leetcode problem #11

---
+++ medium/ContainerWithMostWater.js
@@ -0,0 +1,29 @@
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+
+var maxArea = function(height) {
+  let result = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    let smallestSide = Math.min(height[left], height[right]);
+    let area = smallestSide * (right - left);
+    if (area > result) {
+      result = area;
+    }
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return result;
+};
+
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
