dbb07c0e68263fc345aa61ad0e1df49ad43430f0
Taite Jernigan <taite5100@gmail.com>
2022-01-03 13:01:35 -0800

    Completed leetcode problem #1769

---
+++ medium/MovingBallsToEachBox.js
@@ -0,0 +1,24 @@
+// You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+
+// In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+
+// Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+
+// Each answer[i] is calculated considering the initial state of the boxes.
+
+var minOperations = function(boxes) {
+  let resultArr = [];
+  boxes = boxes.split('');
+  for (let i = 0; i < boxes.length; i++) {
+    let moves = 0;
+    for (let j = 0; j < boxes.length; j++) {
+      if (i !== j && boxes[j] === '1') {
+        moves += Math.abs(i - j);
+      }
+    }
+    resultArr.push(moves);
+  }
+  return resultArr;
+};
+
+console.log(minOperations('001011'));
