9bcb823f144d5d807dd7bead84b49addb080ec57
Taite Jernigan <taite5100@gmail.com>
2022-01-03 13:18:39 -0800

    Completed leetcode problem #2108

---
+++ easy/FirstPalindromicString.js
@@ -0,0 +1,22 @@
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+var firstPalindrome = function(words) {
+  for (let i = 0; i < words.length; i++) {
+    let isPalindrome = true;
+    for (let j = 0; j < (words[i].length) / 2; j++) {
+      if (words[i][j] !== words[i][words[i].length - 1 - j]) {
+        isPalindrome = false;
+        break;
+      }
+    }
+    if (isPalindrome) {
+      return words[i];
+    }
+  }
+  return '';
+};
+
+
+console.log(firstPalindrome(["notapalindrome","racecar"]))
