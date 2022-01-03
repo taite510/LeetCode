c070f1d158cf9e25dddc6ffe3e4575372acaaae8
Taite Jernigan <taite5100@gmail.com>
2022-01-03 12:42:30 -0800

    Completed leetcode problem #2114

---
+++ easy/MaxNumOfWordsInSentence.js
@@ -0,0 +1,19 @@
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+// Return the maximum number of words that appear in a single sentence.
+
+var mostWordsFound = function(sentences) {
+  let maxNum = 0;
+  for (let i = 0; i < sentences.length; i++) {
+    let temp = sentences[i].split(' ');
+    if (temp.length > maxNum) {
+      maxNum = temp.length;
+    }
+  }
+  return maxNum;
+};
+
+
+console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
