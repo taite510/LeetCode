fbedf70461b4db87d6f24b4bedfcc0c58ddc021b
Taite Jernigan <taite5100@gmail.com>
2022-01-05 12:00:07 -0800

    Completed leetcode problem #1480

---
+++ easy/RunningSumArray.js
@@ -0,0 +1,12 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+var runningSum = function(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i] + nums[i - 1];
+  }
+  return nums;
+};
+
+console.log(runningSum([3,1,2,10,-10]))
