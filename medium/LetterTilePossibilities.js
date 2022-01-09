3346c9651b8aedf77e5921dab1088e51abf1c7c2
Taite Jernigan <taite5100@gmail.com>
2022-01-08 17:29:45 -0800

    Completed leetcode problem #1079

---
+++ medium/LetterTilePossibilities.js
@@ -0,0 +1,22 @@
+// You have n  tiles, where each tile has one letter tiles[i] printed on it.
+
+// Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.
+
+var numTilePossibilities = function(tiles) {
+  let obj = {};
+  let tileBuilder = (remaining, current) => {
+    if (current) {
+      obj[current] = true;
+    } else {
+      current = '';
+    }
+    for (let i = 0; i < remaining.length; i++) {
+      temp = remaining.slice(0, i) + remaining.slice(i + 1);
+      tileBuilder(temp, current + remaining[i]);
+    }
+  }
+  tileBuilder(tiles);
+  return Object.keys(obj).length;
+};
+
+console.log(numTilePossibilities("ABCDEF"))
