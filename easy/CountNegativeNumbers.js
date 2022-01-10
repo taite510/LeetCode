891d3ff40350335050b3ea8567938c3b11af5f47
Taite Jernigan <taite5100@gmail.com>
2022-01-09 17:47:34 -0800

    Completed leetcode problem #1351

---
+++ easy/CountNegativeNumbers.js
@@ -0,0 +1,15 @@
+// Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+var countNegatives = function(grid) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] < 0) {
+        count++
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]))
