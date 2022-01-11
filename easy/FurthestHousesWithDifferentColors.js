3e79aa955e7b98c60a0a76b25eb5690f7e402a8c
Taite Jernigan <taite5100@gmail.com>
2022-01-10 21:42:11 -0800

    Completed leetcode problem #2078

---
+++ easy/FurthestHousesWithDifferentColors.js
@@ -0,0 +1,22 @@
+// There are n houses evenly lined up on the street, and each house is beautifully painted.
+
+// You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+
+// Return the maximum distance between two houses with different colors.
+
+// The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+
+var maxDistance = function(colors) {
+  let distance = 1
+  for (let i = 0; i < colors.length; i++) {
+    for (let j = colors.length - 1; j > i; j--) {
+      if (colors[i] !== colors[j] && j - i > distance) {
+        distance = j - i;
+      }
+    }
+  }
+  return distance;
+};
+
+
+console.log(maxDistance([1,1,1,6,1,1,1]))
