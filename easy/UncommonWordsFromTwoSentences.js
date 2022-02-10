6d944806b05a66b5108c7895fa0cf703c288ccaf
Taite Jernigan <taite5100@gmail.com>
2022-02-10 11:54:42 -0800

    Completed leetcode problem #884

---
+++ easy/UncommonWordsFromTwoSentences.js
@@ -0,0 +1,36 @@
+// A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+// A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+// Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+var uncommonFromSentences = function(s1, s2) {
+  let wordBank = {};
+  let resArr = [];
+  s1 = s1.split(' ');
+  s2 = s2.split(' ');
+  let addToWorkBank = function(sentence) {
+    sentence.forEach(word => {
+      if (wordBank[word] === undefined) {
+        wordBank[word] = 1;
+      } else {
+        wordBank[word]++;
+      }
+    })
+  }
+  addToWorkBank(s1);
+  addToWorkBank(s2);
+  for (let key in wordBank) {
+    if (wordBank[key] === 1) {
+      resArr.push(key);
+    }
+  }
+  return resArr;
+};
+
+
+
+let testSentence1 = "apple apple";
+let testSentence2 = "banana";
+
+console.log(uncommonFromSentences(testSentence1, testSentence2))
