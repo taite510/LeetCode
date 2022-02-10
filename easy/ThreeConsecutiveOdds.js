159206b575006a30a4c27e87e5c0ecfeb0dcb99d
Taite Jernigan <taite5100@gmail.com>
2022-02-10 13:08:53 -0800

    Completed leetcode problem #1550

---
+++ easy/ThreeConsecutiveOdds.js
@@ -0,0 +1,20 @@
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+var threeConsecutiveOdds = function(arr) {
+  let oddCounter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      oddCounter = 0;
+    } else {
+      if (oddCounter === 2) {
+        return true;
+      }
+      oddCounter++;
+    }
+  }
+  return false;
+};
+
+let testArr = [1,2,34,3,4,5,7,23,12];
+
+console.log(threeConsecutiveOdds(testArr))
