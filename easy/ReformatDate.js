08dddf3e6c47f8a9adada1615365ea0b36140d46
Taite Jernigan <taite5100@gmail.com>
2022-03-01 11:50:05 -0800

    Completed leetcode problem #1507

---
+++ easy/ReformatDate.js
@@ -0,0 +1,27 @@
+// Given a date string in the form Day Month Year, where:
+
+// Day is in the set {'1st', '2nd', '3rd', '4th', ..., '30th', '31st'}.
+// Month is in the set {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'}.
+// Year is in the range [1900, 2100].
+// Convert the date string to the format YYYY-MM-DD, where:
+
+// YYYY denotes the 4 digit year.
+// MM denotes the 2 digit month.
+// DD denotes the 2 digit day.
+
+var reformatDate = function(date) {
+  let months = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+  date = date.split(' ');
+  let year = date[2]
+  let day, month;
+  if (date[0].length === 3) {
+    day = '0' + date[0][0];
+  } else {
+    day = date[0][0] + date[0][1];
+  }
+  return `${year}-${months[date[1]]}-${day}`
+};
+
+let testDate = "26th May 1960"
+
+console.log(reformatDate(testDate))
