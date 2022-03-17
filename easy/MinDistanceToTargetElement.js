bbc2f0f8eebcc38f4c6db2093ee2bcfa2864b6f4
Taite Jernigan <taite5100@gmail.com>
2022-03-17 12:59:07 -0700

    Completed leetcode problem #1848

---
+++ easy/MinDistanceToTargetElement.js
@@ -0,0 +1,24 @@
+// Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+
+// Return abs(i - start).
+
+// It is guaranteed that target exists in nums.
+
+var getMinDistance = function(nums, target, start) {
+  let position;
+  let i = 1;
+  if (nums[start] === target) {
+    return 0;
+  }
+  while (position === undefined) {
+    if (nums[start + i] === target) {
+      position = start + i;
+    } else if (nums[start - i] === target) {
+      position = start - i;
+    }
+    i++;
+  }
+  return Math.abs(position - start)
+};
+
+console.log(getMinDistance([1,2,3,4,5,6,1], 6, 4))
