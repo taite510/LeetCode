3c1da3978188e8bf57123c8c29907a02ae6143cb
Taite Jernigan <taite5100@gmail.com>
2022-03-31 15:27:13 -0700

    Completed leetcode problem #2114 in python

---
+++ easy/MaxNumOfWordsInSentence.py
@@ -0,0 +1,16 @@
+# A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+# You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+# Return the maximum number of words that appear in a single sentence.
+
+def mostWordsFound(sentences) :
+  max = 0
+  for sentence in sentences :
+    num = len(sentence.split())
+    if num > max :
+      max = num
+  return max
+
+print(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))
+
