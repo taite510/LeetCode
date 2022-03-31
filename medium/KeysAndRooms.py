bb41c05e3dcd210435f8be4092a6cf2a337c7603
Taite Jernigan <taite5100@gmail.com>
2022-03-31 16:10:31 -0700

    Completed leetcode problem #841 in python

---
+++ medium/KeysAndRooms.py
@@ -0,0 +1,17 @@
+# There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+# When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+# Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
+
+def canVisitAllRooms(rooms) :
+  keys = {}
+  def openDoor(door) :
+    keys[door] = True
+    for key in rooms[door] :
+      if key not in keys :
+        openDoor(key)
+  openDoor(0)
+  return len(rooms) == len(keys)
+
+print(canVisitAllRooms([[1],[2],[3],[]]))
