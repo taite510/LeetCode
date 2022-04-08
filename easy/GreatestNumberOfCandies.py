c332c400f14256637248633601c27676bb123c6d
Taite Jernigan <taite5100@gmail.com>
2022-04-08 12:49:02 -0700

    Completed leetcode problem #1431 in python

---
+++ easy/GreatestNumberOfCandies.py
@@ -0,0 +1,17 @@
+# There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+# Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+# Note that multiple kids can have the greatest number of candies.
+
+def kidsWithCandies(candies, extraCandies) :
+  maxCandies = max(candies)
+  arr = []
+  for candy in candies :
+    if candy + extraCandies >= maxCandies :
+      arr.append(True)
+    else :
+      arr.append(False)
+  return arr
+
+print(kidsWithCandies([2,3,5,1,3], 3))
