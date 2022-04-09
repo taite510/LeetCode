c2155cb495b7590a0592c29b8288f3d9a474dec0
Taite Jernigan <taite5100@gmail.com>
2022-04-09 12:56:13 -0700

    Completed leetcode problem #771 in python

---
+++ easy/JewelsAndStones.py
@@ -0,0 +1,11 @@
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+def numJewelsInStones(jewels, stones) :
+  count = 0
+  for letter in jewels :
+    count += stones.count(letter)
+  return count
+
+print(numJewelsInStones('aA', 'aAAbbbb'))
