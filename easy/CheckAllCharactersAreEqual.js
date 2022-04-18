c6e9e002fe0e31f694b683fe6164f361aa503d1f
Taite Jernigan <taite5100@gmail.com>
2022-04-18 12:59:11 -0700

    Completed leetcode problem #1941 in javascript

---
+++ easy/CheckAllCharactersAreEqual.js
@@ -0,0 +1,21 @@
+// Given a string s, return true if s is a good string, or false otherwise.
+
+// A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+var areOccurrencesEqual = function(s) {
+  let bank = {};
+  let num = 0;
+  for (let i = 0; i < s.length; i++) {
+    bank[s[i]] ? bank[s[i]]++ : bank[s[i]] = 1;
+  }
+  for (chara in bank) {
+    if (num && num !== bank[chara]) {
+      return false;
+    } else {
+      num = bank[chara];
+    }
+  }
+  return true;
+};
+
+console.log(areOccurrencesEqual('abcdabcdabcd'));
