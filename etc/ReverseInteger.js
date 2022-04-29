0315edd55c2821f33308c06f52f3069c41080abc
Taite Jernigan <taite5100@gmail.com>
2022-04-28 23:16:54 -0700

    Completed assessment problem

---
+++ etc/ReverseInteger.js
@@ -0,0 +1,18 @@
+// Reverses integer removing any trailing zeros
+
+function solution(N) {
+  var str = ''
+  var enable_print = N % 10;
+  while (N > 0) {
+    if (enable_print == 0 && N % 10 != 0) {
+      enable_print = 1;
+    }
+    if (enable_print >= 1) {
+      str += (N % 10).toString();
+    }
+    N = Math.floor(N / 10);
+  }
+  return Number(str)
+}
+
+console.log(solution(23100))
