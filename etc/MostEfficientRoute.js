bb5c36783c62a96b3283babe47e78936b6897d9d
Taite Jernigan <taite5100@gmail.com>
2022-04-30 18:13:16 -0700

    Completed an assessment problem

---
+++ etc/MostEfficientRoute.js
@@ -0,0 +1,22 @@
+// finds the route pairs with the most efficient route
+
+function routePairs(maxTravelDist, forwardRouteList, returnRouteList) {
+  let pairs = [];
+  let distance = 0;
+  for (let i = 0; i < forwardRouteList.length; i++) {
+      for (let j = 0; j < returnRouteList.length; j++) {
+          let totalDistance = forwardRouteList[i][1] + returnRouteList[j][1];
+          let pair = [forwardRouteList[i][0], returnRouteList[j][0]];
+          if (totalDistance <= maxTravelDist) {
+              if (totalDistance > distance) {
+                  distance = totalDistance;
+                  pairs = [];
+              }
+              pairs.push(pair);
+          }
+      }
+  }
+  return pairs
+}
+debugger
+console.log(routePairs(10000,[[1,3000],[2,5000],[3,7000],[4,10000]],[[1,2000],[2,3000],[3,4000],[4,5000]]))
