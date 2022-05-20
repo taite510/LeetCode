9d77c05007ae59441731d1ae1b692a513e052ce9
Taite Jernigan <taite5100@gmail.com>
2022-05-19 23:06:29 -0700

    Completed leetcode problem #1967 in python

---
+++ easy/SubstringsInStrings.py
@@ -0,0 +1,12 @@
+# Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+# A substring is a contiguous sequence of characters within a string.
+
+def numOfStrings(patterns, word) :
+  count = 0
+  for pattern in patterns :
+    if pattern in word :
+      count += 1
+  return count
+
+print(numOfStrings(['a', 'bc', 'ab', 'd'], 'abc'))
