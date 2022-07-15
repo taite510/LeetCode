3f54a940b8e373a96eb882cc074d7c1204b60157
Taite Jernigan <taite5100@gmail.com>
2022-07-15 14:54:36 -0700

    Completed leetcode problem #2149 in javascript

---
+++ medium/RearrangeElementsBySign.js
@@ -0,0 +1,35 @@
+// You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+// You should rearrange the elements of nums such that the modified array follows the given conditions:
+
+// Every consecutive pair of integers have opposite signs.
+// For all integers with the same sign, the order in which they were present in nums is preserved.
+// The rearranged array begins with a positive integer.
+// Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+const rearrangeArray = function(nums) {
+  let resArr = [];
+  let positive = [];
+  let negative = [];
+  for (let i = 0; i < nums.length; i++) {
+      let num = nums[i];
+      if (num >= 0) {
+          positive.push(num);
+      } else {
+          negative.push(num);
+      }
+  }
+  let j = 0;
+  while (j < positive.length && j < negative.length) {
+      if (j < positive.length) {
+          resArr.push(positive[j]);
+      }
+      if (j < negative.length) {
+          resArr.push(negative[j]);
+      }
+      j++;
+  }
+  return resArr;
+};
+
+console.log(rearrangeArray([3,1,-2,-5,2,-4]))
