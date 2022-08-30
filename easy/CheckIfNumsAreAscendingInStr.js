c26a010a61df1802d4eee900417f51cfd88bc3f3
Taite Jernigan <taite5100@gmail.com>
2022-08-30 15:26:00 -0700

    Completed leetcode problem #2042 in javascript

---
+++ easy/CheckIfNumsAreAscendingInStr.js
@@ -0,0 +1,28 @@
+// A sentence is a list of tokens separated by a single space with no leading or trailing spaces. Every token is either a positive number consisting of digits 0-9 with no leading zeros, or a word consisting of lowercase English letters.
+
+// For example, "a puppy has 2 eyes 4 legs" is a sentence with seven tokens: "2" and "4" are numbers and the other tokens such as "puppy" are words.
+// Given a string s representing a sentence, you need to check if all the numbers in s are strictly increasing from left to right (i.e., other than the last number, each number is strictly smaller than the number on its right in s).
+
+// Return true if so, or false otherwise.
+
+var areNumbersAscending = function(s) {
+  let prevNum = -Infinity;
+  let currNum = '';
+  for (let i = 0; i < s.length + 1; i++) {
+      let char = s[i];
+      if (!isNaN(char) && !isNaN(parseFloat(char))) {
+          currNum += char;
+      } else if (currNum !== '') {
+          currNum = Number(currNum)
+          if (currNum > prevNum) {
+              prevNum = currNum;
+              currNum = '';
+          } else {
+              return false;
+          }
+      }
+  }
+  return true;
+};
+
+console.log(areNumbersAscending('5 is larger than 41'))
