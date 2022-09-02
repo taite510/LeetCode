6e198e2924ca1417efe448877eec16c02a14a367
Taite Jernigan <taite5100@gmail.com>
2022-09-01 18:19:12 -0700

    Completed leetcode problem #2138 in javascript

---
+++ easy/DivideStrIntoGroupsOfSizeK.js
@@ -0,0 +1,31 @@
+// A string s can be partitioned into groups of size k using the following procedure:
+
+// The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.
+// For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+// Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
+
+// Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
+
+const divideString = function(s, k, fill) {
+  const resArr = []
+  const iter = Math.ceil(s.length / k) * k
+  let count = 0
+  let str = ''
+  for (let i = 0; i < iter; i++) {
+      let char = s[i]
+      count++
+      if (s[i]) {
+          str += char
+      } else {
+          str += fill
+      }
+      if (count === k) {
+          resArr.push(str)
+          str = ''
+          count = 0
+      }
+  }
+  return resArr
+};
+
+console.log(divideString('abcdefghij', 3, 'x'))
