d91317653f3ff16f58688a267f279114020c7f66
Taite Jernigan <taite5100@gmail.com>
2022-11-14 15:04:04 -0800

    Completed leetcode problem #485 in python

---
+++ easy/MaxConsecutiveOnes.py
@@ -0,0 +1,17 @@
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+def findMaxConsecutiveOnes(nums):
+    nums.append(0)
+    count = 0
+    maxOnes = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+            if count > maxOnes:
+                maxOnes = count
+        else:
+            count = 0
+    return maxOnes
+
+
+print(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]) == 3)
