ce93480f1f148b58531908b1fafeed16ec506e96
Taite Jernigan <taite5100@gmail.com>
2022-12-02 17:02:37 -0800

    Completed leetcode problem #485 in ruby

---
+++ easy/MaxConsecutiveOnes.rb
@@ -0,0 +1,18 @@
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+def find_max_consecutive_ones(nums)
+  curr = 0
+  max = 0
+  nums.append(0)
+  nums.each do |num|
+      if num == 1
+          curr += 1
+          max = [max, curr].max
+      else
+          curr = 0
+      end
+  end
+  return max
+end
+
+puts find_max_consecutive_ones([0,1,1,0,0,0,1,0]) == 2
